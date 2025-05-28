cdf23946ebbd9bf599a50f961f7ba9f7a9cac75c
Mauricio Artavia Monge <c10743@ECCI107L393338.ecci.ucr.ac.cr>
2025-05-28 10:39:56 -0600

    archivos iniciales login page

---
+++ animalShelter-page/login.cpp
@@ -0,0 +1,6 @@
+#include <iostream>  // Esta es la biblioteca correcta para std::cout
+
+int main() {
+    std::cout << "Hello World" << std::endl;  // std::cout se usa para imprimir en consola
+    return 0;  // Es buena práctica retornar 0 en main
+}
